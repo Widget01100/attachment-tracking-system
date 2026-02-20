1265373f669c7659de27e2cbc7c343a174b53b2d
widget01100 <francistom107@gmail.com>
2026-02-20 20:10:03 +0300

    Add core/urls.py file

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+﻿from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('profile/', views.profile, name='profile'),
+    path('apply/', views.apply_attachment, name='apply'),
+    path('logbook/<int:attachment_id>/', views.logbook, name='logbook'),
+    path('evaluate/<int:attachment_id>/', views.evaluate, name='evaluate'),
+]
